a853fd468cc13ce4264eae63df3305f4a5a54739
Alexandra Gaurit <alexandragaurit@outlook.fr>
2021-10-17 11:05:00 +0200

    last commit

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require('mongoose');
+const helmet = require("helmet");
+require('dotenv').config();
 const path = require('path');
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauces');
-
-mongoose.connect('mongodb+srv://piiquanteAGP6:My46ZLZKRtKJhPa@cluster0.vrhop.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+console.log(process.env);
+mongoose.connect( process.env.MONGODB_URI,
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -24,6 +26,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use(helmet());
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
